6cedfa6801834c0476a18dee472630cd23be3cab
Andrew Crites <acrites@mobiquityinc.com>
2016-11-09 16:02:07 -0500

    Removing unnecessary constructs from lambda function execution

--- infrastructure/apex/functions/flow_iot/index.js
+++ infrastructure/apex/functions/flow_iot/index.js
@@ -1,6 +1,6 @@
 import λ from "apex.js";
 import updateDeviceShadow from './update-device-shadow';
 
-export default λ(async event => {
-    return await updateDeviceShadow(process.env.IOT_ENDPOINT, process.env.IOT_THING_NAME, event.data);
-});
+export default λ(event =>
+    updateDeviceShadow(process.env.IOT_ENDPOINT, process.env.IOT_THING_NAME, event.data)
+);
